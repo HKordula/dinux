83e51791e8721438df2eeb45db9da2826a0597e6
Michał Mistarz <michal.mistarz02@gmail.com>
2025-04-30 20:26:05 +0200

    Added button to display dinos

--- frontend/script.js
+++ frontend/script.js
@@ -14,6 +14,12 @@ async function fetchAndDisplayDinosaurs() {
         dinosaurs.forEach(dino => {
             const listItem = document.createElement('li'); 
             listItem.textContent = dino.name;
+
+            const button = document.createElement('button');
+            button.textContent = 'Details';
+            button.addEventListener('click', () => fetchDinosaurDetails(dino.id));
+
+            listItem.appendChild(button);
             list.appendChild(listItem);
         });
 
@@ -21,6 +27,33 @@ async function fetchAndDisplayDinosaurs() {
     } catch (error) {
         console.error('Error:', error);
     }
+}
+
+async function fetchDinosaurDetails(id) {
+   try {
+    const response = await fetch(`http://localhost:3000/dinos/${id}`);
+    if (!response.ok){
+        throw new Error('Failed to fetch dinosaur details');
+    }
+
+    const dino = await response.json();
+
+    const details = document.createElement('div');
+    details.innerHTML = `
+        <h3>${dino.name}</h3>
+        <p>ID: ${dino.id}</p> 
+        <p>diet: ${dino.diet}</p> 
+    `;
+
+    const existingDetails = document.querySelector('.dino-details');
+    if (existingDetails) {
+        existingDetails.remove();
+    }
+    details.classList.add('dino-details');
+    container.appendChild(details);
+   } catch (error) {
+    console.error('Error: ', error);
+   }
 }
 
 showDinosaursBtn.addEventListener('click', fetchAndDisplayDinosaurs);
